39b4f812aa084d1a5fb9927ecbf9baddeeca5ca2
Jeremy W. Rowe <jeremy.w.rowe@gmail.com>
2014-06-29 22:42:06 -0400

    wip - have a working customer record just need to test it

--- Chargify.Java/src/main/java/com/chargify/core/resources/Customer.java
+++ Chargify.Java/src/main/java/com/chargify/core/resources/Customer.java
@@ -24,9 +24,11 @@
 
 package com.chargify.core.resources;
 
-import org.simpleframework.xml.Default;
-import org.simpleframework.xml.DefaultType;
-import org.simpleframework.xml.Root;
+import com.chargify.core.Client;
+import com.chargify.core.ClientFactory;
+import com.chargify.core.Configuration;
+import org.simpleframework.xml.*;
+import org.simpleframework.xml.core.Persister;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.ArrayList;
@@ -35,15 +37,46 @@ import java.util.ArrayList;
 @Default(DefaultType.FIELD)
 public class Customer extends Resource {
 
-    public Customer find(String reference) {
+    @Element private int id;
+    @Element private String email = "";
+    @Element(name="first_name") private String firstName = "";
+    @Element(name="last_name")  private String lastName = "";
+
+    @Element(required=false) private String organization = "";
+    @Element(required=false) private String reference = "";
+    @Element(required=false) private String address = "";
+    @Element(name="address_2", required=false) private String address2 = "";
+    @Element(required=false) private String city = "";
+    @Element(required=false) private String state = "";
+    @Element(required=false) private String zip = "";
+    @Element(required=false) private String country = "";
+    @Element(required=false) private String phone = "";
+
+    @Transient private boolean verified;
+    @Transient private String portalCustomerCreatedAt = "";
+    @Transient private String portalInviteLastSentAt = "";
+    @Transient private String portalInviteLastAcceptedAt = "";
+    @Transient private String createdAt = "";
+    @Transient private String updatedAt = "";
+
+    public static Customer find(int id) throws Exception {
+        Client client = ClientFactory.build();
+        // TODO: request should be returned instead of response
+        //       so that we can check the return code. and respond
+        //       with errors.
+        String response = client.get("customers/" + id);
+        Serializer deserializer = new Persister();
+        return deserializer.read(Customer.class, response);
+    }
+
+    public static Customer find(String reference) {
+
         throw new NotImplementedException();
     }
 
-    public Customer find(int id) {
-        throw new NotImplementedException();
-    }
 
-    public ArrayList<Customer> all() {
+
+    public static ArrayList<Customer> all() {
         throw new NotImplementedException();
     }
 
---
+++ Chargify.Java/src/test/java/com/chargify/core/resources/CustomerTest.java
@@ -0,0 +1,12 @@
+package com.chargify.core.resources;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+
+    @Test
+    public void testFind() throws Exception {
+    }
+}
