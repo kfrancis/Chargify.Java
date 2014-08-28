5682f1c1c97ac70d76a94ff299571a87634e2dc4
Kori Francis <byter@me.com>
2014-08-28 00:22:14 -0400

    Fill out more subscription

--- Chargify.Java/src/main/java/com/chargify/core/resources/Subscription.java
+++ Chargify.Java/src/main/java/com/chargify/core/resources/Subscription.java
@@ -30,10 +30,12 @@ import com.chargify.core.http.ListResponse;
 import com.chargify.core.http.Response;
 import com.chargify.core.resources.collections.SubscriptionList;
 import com.github.kevinsawicki.http.HttpRequest;
+import java.util.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.simpleframework.xml.*;
 import org.simpleframework.xml.core.Persister;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-import java.util.*;
 
 /**
  * @see <a href="https://docs.chargify.com/api-subscriptions">https://docs.chargify.com/api-subscriptions</a>
@@ -43,40 +45,72 @@ import java.util.*;
 @Default(DefaultType.FIELD)
 public class Subscription extends Resource {
 
-    @Element private Integer id;
-    @Element(name="activated_at", required=false) private Date activatedAt = null;
-    @Element(name="balanace_in_cents", required=false) private Integer balanceInCents = null;
-    @Element(name="cancel_at_end_of_period", required=false) private Boolean cancelAtEndOfPeriod = null;
-    @Element(name="canceled_at", required=false) private Date canceledAt = null;
-    @Element(name="cancellation_message", required=false) private String cancellationMessage = null;
-    @Element(name="created_at", required=false) private Date createdAt = null;
-    @Element(name="current_period_ends_at", required=false) private Date currentPeriodEndsAt = null;
-    @Element(name="expires_at", required=false) private Date expiresAt = null;
-    @Element(name="next_assessment_at", required=false) private Date nextAssessmentAt = null;
-    @Element(name="payment_collection_method", required=false) private String paymentCollectionMethod = null;
-    @Element(required=false) private String state = null;
-    @Element(name="trial_ended_at", required=false) private Date trialEndedAt = null;
-    @Element(name="trial_started_at", required=false) private Date trialStartedAt = null;
+    @Getter @Setter @Element(required=false, name="cancel_at_end_of_period") private Boolean cancelAtEndOfPeriod = null;
+    @Getter @Setter @Element(required=false, name="canceled_at") private Date canceledAt = null;
+    @Getter @Setter @Element(required=false, name="cancellation_message") private String cancellationMessage = null;
+    @Getter @Setter @Element(required=false, name="created_at") private Date createdAt = null;
+    @Getter @Setter @Element(required=false, name="current_period_ends_at") private Date currentPeriodEndsAt = null;
+    @Getter @Setter @Element(required=false, name="expires_at") private Date expiresAt = null;
+    @Getter @Setter @Element(required=false, name="next_billing_at") private Date nextBillingAt = null;
+    @Getter @Setter @Element(required=false, name="payment_collection_method") private String paymentCollectionMethod = null;
+    @Getter @Setter @Element(required=false) private String state = null;
+    @Getter @Setter @Element(required=false, name="trial_ended_at") private Date trialEndedAt = null;
+    @Getter @Setter @Element(required=false, name="trial_started_at") private Date trialStartedAt = null;
+    @Getter @Setter @Element(required=false, name="product_handle") private String productHandle = null;
+    @Getter @Setter @Element(required=false, name="product_id") private Integer productId = null;
+    @Getter @Setter @Element(required=false, name="customer_id") private Integer customerId = null;
+    @Getter @Setter @Element(required=false, name="customer_reference") private String customerReference = null;
+    @Getter @Setter @Element(required=false, name="payment_profile_id") private Integer paymentProfileId = null;
+    @Getter @Setter @Element(required=false, name="vat_number") private String vatNumber = null;
+    @Getter @Setter @Element(required=false, name="coupon_code") private String couponCode = null;
+    @Getter @Setter @Element(required=false, name="product_change_delayed") private Boolean productChargeDelayed = null;
+    
+    /* Output only */
+    @Setter @Element(required=false) private Integer id = null;
+    @Setter @Element(required=false, name="activated_at") private Date activatedAt = null;
+    @Setter @Element(required=false, name="next_assessment_at") private Date nextAssessmentAt = null;
+    @Setter @Element(required=false, name="balanace_in_cents") private Integer balanceInCents = null;
 
+    /**
+     * Represents the subscription record as a hash
+     * @return a hash of all the subscription attributes
+     */
+    @Override
+    public HashMap<String, String> asHash() {
+        HashMap<String, String> hash = new HashMap<>();
+        guard("subscription[id]", this.getId(), hash);
+        guard("subscription[product_handle]", this.getProductHandle(), hash);
+        guard("subscription[product_id]", this.getProductId(), hash);
+        guard("subscription[customer_id]", this.getCustomerId(), hash);
+        guard("subscription[customer_reference]", this.getCustomerReference(), hash);
+        guard("subscription[payment_profile_id]", this.getPaymentProfileId(), hash);
+        guard("subscription[cancel_at_end_of_period]", this.getCancelAtEndOfPeriod(), hash);
+        guard("subscription[cancellation_message]", this.getCancellationMessage(), hash);
+        guard("subscription[next_billing_at]", this.getNextBillingAt(), hash);
+        guard("subscription[payment_collection_method]", this.getPaymentCollectionMethod(), hash);
+        guard("subscription[vat_number]", this.getVatNumber(), hash);
+        guard("subscription[coupon_code]", this.getCouponCode(), hash);
+        guard("subscription[product_change_delayed]", this.getProductChargeDelayed(), hash);
+        return hash;
+    }
+    
+    /**
+     * Creates or updates a record in Chargify
+     * @return A subscription response object from the request
+     * @throws Exception
+     */
     public Response<Subscription> save() throws Exception {
        return _save(ClientFactory.build());
     }
-
-    public Response<Subscription> _save(Client client) throws Exception {
-        HttpRequest request = this.isPersisted() ?
-                client.put("subscriptions/" + id, this.asHash()) :
-                client.post("subscriptions", this.asHash());
-
-        return new Response<Subscription>(request.code(), request.body(), Subscription.class);
-    }
-
-    public Response<Subscription> cancel() throws Exception {
-        return _cancel(ClientFactory.build());
-    }
-
-    public Response<Subscription> _cancel(Client client) throws Exception {
-        HttpRequest request =  client.delete("subscriptions/" + id);
-        return new Response<Subscription>(request.code(), request.body(), Subscription.class);
+    
+    /**
+     * Cancel a subscription
+     * @param   id The Chargify id of the subscription
+     * @return  The subscription response object from the request
+     * @throws Exception
+     */
+    public Response<Subscription> cancel(int id) throws Exception {
+        return _cancel(ClientFactory.build(), id);
     }
 
     /**
@@ -86,7 +120,16 @@ public class Subscription extends Resource {
      * @throws Exception
      */
     public static Response<Subscription> find(int id) throws Exception {
-        return find(ClientFactory.build(), id);
+        return _find(ClientFactory.build(), id);
+    }
+    
+    /**
+     * Retrieve all subscriptions
+     * @return              The list of subscriptions
+     * @throws Exception
+     */
+    public static ListResponse<Subscription, SubscriptionList> all() throws Exception {
+        return _all(ClientFactory.build());
     }
     
     /**
@@ -96,7 +139,7 @@ public class Subscription extends Resource {
      * @return              The details of the subscription
      * @throws Exception
      */
-    public static Response<Subscription> find(Client client, int id) throws Exception {
+    public static Response<Subscription> _find(Client client, int id) throws Exception {
         HttpRequest request = client.get("subscriptions/" + id);
         return new Response<Subscription>(request.code(), request.body(), Subscription.class);
     }
@@ -107,20 +150,35 @@ public class Subscription extends Resource {
      * @return              The list of subscriptions
      * @throws Exception
      */
-    public static ListResponse<Subscription, SubscriptionList> all(Client client) throws Exception {
+    public static ListResponse<Subscription, SubscriptionList> _all(Client client) throws Exception {
         HttpRequest request = client.get("subscriptions");
         return new ListResponse<Subscription, SubscriptionList>(request.code(), request.body(), Subscription.class, SubscriptionList.class);
     }
     
     /**
-     * Retrieve all subscriptions
-     * @return              The list of subscriptions
+     *
+     * @param client    The communication client
+     * @param id        The Chargify id of the subscription
+     * @return          The details of the subscription as a result of the cancellation
      * @throws Exception
      */
-    public static ListResponse<Subscription, SubscriptionList> all() throws Exception {
-        return all(ClientFactory.build());
+    public Response<Subscription> _cancel(Client client, int id) throws Exception {
+        HttpRequest request = client.delete("subscriptions/" + id);
+        return new Response<Subscription>(request.code(), request.body(), Subscription.class);
     }
     
+    /**
+     *
+     * @param client    The communication client
+     * @return          The details of the subscription as a result of the save
+     * @throws Exception
+     */
+    public Response<Subscription> _save(Client client) throws Exception {
+        HttpRequest request = this.isPersisted() ?
+                client.put("subscriptions/" + id, this.asHash()) :
+                client.post("subscriptions", this.asHash());
+        return new Response<Subscription>(request.code(), request.body(), Subscription.class);
+    }
 
     @Override public boolean canEqual(Object other) {
         return (other instanceof Subscription);
